decb37b8d8f39f691d6a73bf9c6b61f65f5cdb6f
fred-fittywebdev <frederic-guerra_student2021@wilder.school>
2021-12-02 14:06:11 +0100

    Ajout des différents fichiers

---
+++ app.js
@@ -0,0 +1,21 @@
+// import
+const express = require('express')
+const app = express()
+const port = 8080
+
+
+//static files
+app.use(express.static('public'))
+app.use('/css', express.static(__dirname + 'public/css'))
+app.use('/js', express.static(__dirname + 'public/js'))
+app.use('/img', express.static(__dirname + 'public/img'))
+
+//set views
+app.set('views', './views')
+app.set('view engine', 'ejs')
+
+app.get('', (req, res) => {
+    res.render('index', {text: 'This is ejs'})
+})
+//listen to the port 8080
+app.listen(port, () => console.info(`Listening on port ${port}`))
